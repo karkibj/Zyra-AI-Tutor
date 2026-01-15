aeb5138f3b7106697ee6fc9a0573260d07519a75
karkibj <karkibj341@gmail.com>
2026-01-15 10:00:00 +0545

    refactor: update base model for new schema design

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,6 +1,4 @@
-"""
-SQLAlchemy Declarative Base
-"""
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
